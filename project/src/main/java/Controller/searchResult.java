a797743a9d6b3d19d4b82456cc5690c9a911feb9
Jeonghan Oh <myown1209@gmail.com>
2023-04-11 16:35:13 +0900

    Commit_2023_04_11

--- project/src/main/java/Controller/searchResult.java
+++ project/src/main/java/Controller/searchResult.java
@@ -1,6 +1,21 @@
 package Controller;
 
+import DB.DB_Conn;
+
 public class searchResult {
+	DB_Conn db;
 	
+	public searchResult(String query) {
+		db = new DB_Conn();
+		
+		
+	}
 	
+	public String getMenu(String query) {
+		String ret="";
+		
+		
+		
+		return ret;
+	}
 }
