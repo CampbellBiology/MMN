40a5b42c9a51f4ed280248578593cd1b77c82258
Jeonghan Oh <myown1209@gmail.com>
2023-04-11 18:00:02 +0900

    Commit_2023_04_11_PM_05_59

--- project/src/main/java/Controller/searchResult.java
+++ project/src/main/java/Controller/searchResult.java
@@ -3,28 +3,30 @@ package Controller;
 import java.util.ArrayList;
 
 import DB.DB_Conn;
+import DataClass.storeData;
+import DataClass.tagData;
 
 public class searchResult {
 	DB_Conn db;
+	ArrayList <storeData> storeInfo;
+	ArrayList <tagData> tagInfo;
 	
 	public searchResult(String query) {
 		db = new DB_Conn();
-		
-		
+		calcStoreInfo(query);
+		calcTagInfo(query);
 	}
 	
-	public ArrayList<String> getStoreInfo(String query) {
-		ArrayList <String> ret = new ArrayList<>();
+	public void calcStoreInfo(String query) {
+		storeInfo = db.getStoreInfo(query);
+		ArrayList <storeData> tmp = db.getMenuInfo(query);
 		
-		
-		
-		return ret;
+		for(int i=0;i<tmp.size();i++) {
+			storeInfo.add(tmp.get(i));
+		}
 	}
 	
-	public ArrayList<String> getTagInfo(String query) {
-		ArrayList <String> ret = new ArrayList<>();
-		
-		
-		return ret;
+	public void calcTagInfo(String query) {
+		tagInfo = db.getTagInfo(query);
 	}
 }
